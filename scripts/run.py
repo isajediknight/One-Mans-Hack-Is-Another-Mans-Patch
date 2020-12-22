da87065d683c4099f85e20666483f9d3e303b15c
isajediknight <luke@isajediknight.com>
2020-12-22 01:08:18 -0600

    Adding logic to detect if results are paginated

--- scripts/run.py
+++ scripts/run.py
@@ -56,11 +56,9 @@ elif((OS_TYPE == 'linux') or (OS_TYPE == 'macintosh')):
 if((OS_TYPE == 'linux')):
     sys.path.insert(0,'../classes/')
     sys.path.insert(0,MODULES_DIR)
-    #sys.path.insert(0,MODULES_GITHUB_DIR)
 elif((OS_TYPE == 'windows')):
     sys.path.insert(0,'..\\classes\\')
     sys.path.insert(0,MODULES_DIR)
-    #sys.path.insert(0,MODULES_GITHUB_DIR)
 
 # < --- Begin Custom Classes Import --- >
 # Custom Colors for printing to the screen
@@ -72,28 +70,52 @@ from reuseable_methods import parse_args
 from benchmark import *
 # < ---  End  Custom Classes Import --- >
 
+# < ---     Begin Module Import     --- >
 from github import Github
 from github import PullRequest
+# < ---      End  Module Import     --- >
 
+# Benchmark all the things
 runtime = Benchmark()
 
-time.sleep(3)
-
-print("Hello World!")
-
 # To Debug: parse_args('dict',True)
-my_args = parse_args()
+my_args = parse_args('dict',False,[],['-user','-password'],False,['-password'])
 if('-user' in list(my_args.keys())):
     user = my_args['-user'].value
 
 if ('-password' in list(my_args.keys())):
     password = my_args['-password'].value
 
-#g = Github(user, password)
+g = Github(user, password)
 #monero-project/monero
 
 #repo = g.get_repo("PyGithub/PyGithub")
-#repo = g.get_repo("monero-project/monero")
+repo_name = "monero-project/monero"
+repo = g.get_repo(repo_name)
+
+state='closed'
+sort='created'
+base='master'
+# Get open pull
+pulls = repo.get_pulls(state, sort, base)
+
+result_type = 'unknown'
+if("<class 'github.PaginatedList.PaginatedList'>" == str(type(pulls))):
+    print("There are " + str(pulls.totalCount) + " " + state + " Pull Requests in " + repo_name)
+    result_type = 'PaginatedList'
+    final_paginated_page = int(pulls._getLastPageUrl().split("page=")[1])
+else:
+    print("There are " + str(len(pulls)) + " " + state + " Pull Requests in " + repo_name)
+    result_type = 'list'
+
+if(result_type == 'PaginatedList'):
+    one_result = pulls.get_page(0)
+    print(str(pulls._getLastPageUrl().split("page=")[1]))
+    final_paginated_page = 0
+
+#print("There are " + str(len(pulls)) + " " + state + " Pull Requests in " + repo_name)
+
+#print(pulls[0])
 
 runtime.stop()
-print(runtime.seconds_to_human_readable(runtime.get_seconds()))
+print("Program Runtime: "+runtime.seconds_to_human_readable(runtime.get_seconds()))
